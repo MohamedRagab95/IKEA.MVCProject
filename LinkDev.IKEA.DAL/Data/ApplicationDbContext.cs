cdb21b5a1111335045f0e1b84320aa6de03c6125
Mohamed Ragab <mohamed.ragab787@gmail.com>
2024-09-14 03:53:37 +0300

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ LinkDev.IKEA.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Data
+{
+    internal class ApplicationDbContext :DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; Database= IKEA ; Trusted_Connection = True;");
+        }
+    }
+}
